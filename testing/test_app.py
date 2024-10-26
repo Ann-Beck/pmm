b8478ddd4fab361cee63b02a14c14767e0c9e87d
Clara Dopler <34902890+ClaraDopler@users.noreply.github.com>
2024-10-25 20:00:51 -0600

    ClaraDopler making it better

--- testing/test_app.py
+++ testing/test_app.py
@@ -4,5 +4,8 @@ from packaging.version import parse as parse_version
 from pytest_mqtt import __version__
 
 
+
+# just a random comment
+
 def test_app_version():
     assert isinstance(parse_version(__version__), Version)
