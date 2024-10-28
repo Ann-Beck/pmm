32af068a76b25ca6f57c1da957b7300feb872098
Clara Dopler <34902890+ClaraDopler@users.noreply.github.com>
2024-10-28 09:01:01 -0600

    C1028.0900 minor fixes

--- testing/test_app.py
+++ testing/test_app.py
@@ -3,10 +3,7 @@ from packaging.version import parse as parse_version
 
 from pytest_mqtt import __version__
 
-
-
-# just a random comment
-# is this better?
+# this is a simple test of the versions
 
 def test_app_version():
     assert isinstance(parse_version(__version__), Version)
